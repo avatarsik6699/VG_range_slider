4f865e9ad307f617472722fd336670f2c18ab548
avatarsik <avatarsik6699@gmail.com>
2020-10-23 17:44:32 +0700

    Made the final configuration of the webpack. Added loaders and compilators for the pug, sass, ts. Added postcssconfig,tsconfig

---
+++ src/index.ts
@@ -0,0 +1,3 @@
+const username: string = 'Father';
+const number: number = 25;
+const result = 555;
--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,71 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
+// variables---------------------------------------------------
+const isProd = process.env.NODE_ENV === 'production';
+const isDev = !isProd;
+// helper functions--------------------------------------------
+const optimization = () => {
+  const config = {
+    moduleIds: 'deterministic',
+    runtimeChunk: 'single',
+    splitChunks: {
+      cacheGroups: {
+        vendor: {
+          test: /[\\/]node_modules[\\/]/,
+          name: 'vendors',
+          chunks: 'all',
+        }
+      }
+    }
+  }
+
+  if (isProd) {
+    config.minimize = true,
+    config.minimizer = [
+      new TerserPlugin(),
+      new CssMinimizerPlugin({
+        exclude: /node_modules/,
+      }),
+    ]
+  }
+  return config
+}
 // loaders-----------------------------------------------------
-const css = {
-  test: /\.css$/,
+const pug = {
+  test: /\.pug$/,
+  include: path.resolve(__dirname, 'src'),
+  use: [{
+    loader: 'pug-loader',
+    options: {
+        root: path.resolve(__dirname, 'src'),
+    }
+  }]
+};
+const sass = {
+  test: /\.(css|sass|scss)$/i,
+  include: path.resolve(__dirname, 'src'),
   use: [
-    'style-loader',
-    'css-loader',
+    isDev ? 'style-loader' : { 
+    loader: MiniCssExtractPlugin.loader, 
+      options: {
+          hmr: isDev,
+          reloadAll: true,
+      },
+    },
+    { loader: 'css-loader', options: {sourceMap: true} },
+    { loader: 'postcss-loader', options: { sourceMap: true } },
+    { loader: 'resolve-url-loader' },
+    { loader: 'sass-loader', options: {sourceMap: true} },
   ]
 };
 const images = {
   test: /\.(png|svg|jpg|gif)$/,
+  include: path.resolve(__dirname, 'src'),
   use: [
     'file-loader',
     {
@@ -36,18 +92,62 @@ const images = {
 };
 const fonts = {
   test: /\.(woff|woff2|eot|ttf|otf)$/,
+  include: path.resolve(__dirname, 'src'),
   use: [
     'file-loader',
   ],
 };
+const ts = {
+  test: /\.tsx?$/,
+  loader: 'ts-loader',
+  exclude: /node_modules/,
+  options: {
+    configFile: isProd ? 'tsconfig.prod.json' : 'tsconfig.json',
+    transpileOnly: true,
+  },
+}
 // config------------------------------------------------------
 module.exports = {
-  entry: './src/index.js',
+  mode: isProd ? 'production' : 'development',
+  entry: {
+    app: './src/index.ts'
+  },
   output: {
-    filename: 'bundle.js',
+    filename: '[name].[contenthash].js',
     path: path.resolve(__dirname, 'dist')
   },
   module: {
-    rules: [css, images, fonts],
+    rules: [ts, sass, images, fonts, pug],
   },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin({
+      filename: isProd ? '[name]/style.[contenthash].css' : '[name]/style.css',
+    }),
+    new HtmlWebpackPlugin({
+      title: 'Slider page',
+      filename: 'index.html',
+      template: path.resolve(__dirname, 'src/index.pug'),
+      minify: {
+        removeComments: isProd,
+        collapseWhiteSpace: isProd,
+        removeRedundantAttributes: isProd,
+        useShortDoctype: isProd
+      }
+    })
+  ],
+  devtool: isProd ? 'source-map' : 'eval-cheap-module-source-map',
+  devServer: {
+    contentBase: './dist',
+    compress: true,
+    hot: isDev,
+    port: 8000,
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js'],
+    alias: {
+      root: path.resolve(__dirname, 'src/'),
+    }
+  },
+  optimization: optimization(), 
 }
